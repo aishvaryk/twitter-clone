c979be8d18544e911e09bf3ec5a139b8c1d740ed
Aishvary Khandelwal <aishvarykhandelwal@gmail.com>
2022-05-15 17:38:56 +0530

    created folder structure and added MUI theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
-import '../styles/globals.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../src/theme';
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
